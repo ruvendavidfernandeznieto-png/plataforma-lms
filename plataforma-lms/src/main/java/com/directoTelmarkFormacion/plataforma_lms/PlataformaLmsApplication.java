88372d19cf25c5bfbca2a6b26a0c455b7a03212f
ruben.dfn <ruvendavidfernandeznieto@gmail.com>
2026-01-23 13:31:27 +0100

    Se ha creado el proyecto

---
+++ plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/PlataformaLmsApplication.java
@@ -0,0 +1,13 @@
+package com.directoTelmarkFormacion.plataforma_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaLmsApplication.class, args);
+	}
+
+}
