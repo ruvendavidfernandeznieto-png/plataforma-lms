2b87df42b6f945a774a8a0ae958b5619f2489450
ruben.dfn <ruvendavidfernandeznieto@gmail.com>
2026-01-29 08:33:34 +0100

    - Añadida dependencia 'spring-boot-starter-thymeleaf' en pom.xml. - Corregido CourseController para devolver la vista HTML correcta. - Verificado funcionamiento de la tabla de cursos en el navegador.

--- plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/PlataformaLmsApplication.java
+++ plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/PlataformaLmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlataformaLmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlataformaLmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaLmsApplication.class, args);
+    }
 
 }
--- plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/controller/CourseController.java
+++ plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/controller/CourseController.java
@@ -2,7 +2,7 @@ package com.directoTelmarkFormacion.plataforma_lms.controller;
 
 import com.directoTelmarkFormacion.plataforma_lms.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; // <--- OJO: No puede ser @RestController
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/courses")
 public class CourseController {
+
     @Autowired
     private CourseService courseService;
 
     @GetMapping
-    public String listarCursos(Model model){
-        model.addAttribute("cursos", courseService.obtenerTodos());
-        return "course-list";
+    public String listarCursos(Model model) {
+       model.addAttribute("cursos", courseService.obtenerTodos());
+       return "course-list";
     }
 }
