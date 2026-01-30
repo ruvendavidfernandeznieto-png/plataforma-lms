8cbc6badfb98d617a7878f5986ff0c179451112b
ruben.dfn <ruvendavidfernandeznieto@gmail.com>
2026-01-30 13:17:12 +0100

    -Se ha cambiado la estética de los menús disponibles

--- plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/controller/CourseController.java
+++ plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/controller/CourseController.java
@@ -1,10 +1,12 @@
 package com.directoTelmarkFormacion.plataforma_lms.controller;
 
+import com.directoTelmarkFormacion.plataforma_lms.model.Course;
 import com.directoTelmarkFormacion.plataforma_lms.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller; // <--- OJO: No puede ser @RestController
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,12 +24,12 @@ public class CourseController {
 
     @GetMapping("/new")
     public String mostrarFormulario(Model model){
-        model.addAttribute("curso", new com.directoTelmarkFormacion.plataforma_lms.model.Course());
+        model.addAttribute("course", new com.directoTelmarkFormacion.plataforma_lms.model.Course());
         return "course-form";
     }
 
     @org.springframework.web.bind.annotation.PostMapping("/save")
-    public String guardarCurso(@org.springframework.web.bind.annotation.ModelAttribute("curso") com.directoTelmarkFormacion.plataforma_lms.model.Course course){
+    public String guardarCurso(@org.springframework.web.bind.annotation.ModelAttribute("course") com.directoTelmarkFormacion.plataforma_lms.model.Course course){
         courseService.guardarCurso(course);
         return "redirect:/courses";
     }
@@ -35,13 +37,25 @@ public class CourseController {
     @GetMapping("/edit/{id}")
     public String editarFormulario(@org.springframework.web.bind.annotation.PathVariable Long id, Model model){
         com.directoTelmarkFormacion.plataforma_lms.model.Course cursoExistente = courseService.obtenerPorId(id).orElse(null);
-        model.addAttribute("curso", cursoExistente);
+        model.addAttribute("course", cursoExistente);
         return "course-form";
     }
 
     @GetMapping("/delete/{id}")
     public String eliminarCurso(@org.springframework.web.bind.annotation.PathVariable Long id){
         courseService.borrarCurso(id);
+        return "redirect:/courses";
+    }
+
+    @GetMapping("/courses/new")
+    public String showCreateCourseForm(Model model){
+       model.addAttribute("course", new Course());
+       return "course-form";
+    }
+
+    @GetMapping("/courses/save")
+    public String saveCourse(@ModelAttribute("course")Course course){
+        courseService.guardarCurso(course);
         return "redirect:/courses";
     }
 }
--- plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/service/CustomUserDetailsService.java
+++ plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/service/CustomUserDetailsService.java
@@ -17,7 +17,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        // 1. Buscamos el usuario en TU base de datos por email
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con email: " + email));
 
