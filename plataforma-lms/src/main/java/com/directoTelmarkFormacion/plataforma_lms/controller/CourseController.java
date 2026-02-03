547d3ce5ba9b1a654bee8ee59e4fad11de4cc1f0
ruben.dfn <ruvendavidfernandeznieto@gmail.com>
2026-02-03 08:56:49 +0100

    -Se ha implementado la función de crear temas y subir archivos

--- plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/controller/CourseController.java
+++ plataforma-lms/src/main/java/com/directoTelmarkFormacion/plataforma_lms/controller/CourseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/courses")
@@ -16,54 +17,46 @@ public class CourseController {
 
     @GetMapping
     public String listarCursos(Model model) {
-       model.addAttribute("cursos", courseService.obtenerTodos());
-       return "course-list";
+        model.addAttribute("cursos", courseService.obtenerTodos());
+        return "course-list";
     }
 
     @GetMapping("/new")
     public String mostrarFormulario(Model model){
-        model.addAttribute("course", new com.directoTelmarkFormacion.plataforma_lms.model.Course());
+        model.addAttribute("course", new Course());
         return "course-form";
     }
 
-    @org.springframework.web.bind.annotation.PostMapping("/save")
-    public String guardarCurso(@org.springframework.web.bind.annotation.ModelAttribute("course") com.directoTelmarkFormacion.plataforma_lms.model.Course course){
+    @PostMapping("/save")
+    public String guardarCurso(@ModelAttribute("course") Course course){
         courseService.guardarCurso(course);
         return "redirect:/courses";
     }
 
     @GetMapping("/edit/{id}")
-    public String editarFormulario(@org.springframework.web.bind.annotation.PathVariable Long id, Model model){
-        com.directoTelmarkFormacion.plataforma_lms.model.Course cursoExistente = courseService.obtenerPorId(id).orElse(null);
+    public String editarFormulario(@PathVariable Long id, Model model){
+        Course cursoExistente = courseService.obtenerPorId(id).orElse(null);
         model.addAttribute("course", cursoExistente);
         return "course-form";
     }
 
     @GetMapping("/delete/{id}")
-    public String eliminarCurso(@org.springframework.web.bind.annotation.PathVariable Long id){
+    public String eliminarCurso(@PathVariable Long id){
         courseService.borrarCurso(id);
         return "redirect:/courses";
     }
 
-    @GetMapping("/courses/new")
-    public String showCreateCourseForm(Model model){
-       model.addAttribute("course", new Course());
-       return "course-form";
-    }
-
-    @GetMapping("/courses/save")
-    public String saveCourse(@ModelAttribute("course")Course course){
-        courseService.guardarCurso(course);
-        return "redirect:/courses";
-    }
     @PostMapping("/{courseId}/add-module")
-    public String addModule(@PathVariable Long courseId,@RequestParam("title")String title){
-        courseService.guardarModulo(courseId,title);
-        return "redirect:/courses/edit" + courseId;
+    public String addModule(@PathVariable Long courseId, @RequestParam("title") String title){
+        courseService.guardarModulo(courseId, title);
+
+        return "redirect:/courses/edit/" + courseId;
     }
 
     @PostMapping("/modules/{moduleId}/add-lesson")
-    public String addLesson(@PathVariable Long moduleId, @RequestParam("title")String title,@RequestParam("file")org.springframework.web.multipart.MultipartFile file){
+    public String addLesson(@PathVariable Long moduleId,
+                            @RequestParam("title") String title,
+                            @RequestParam("file") MultipartFile file){
         try {
             courseService.guardarLeccion(moduleId, title, file);
         } catch (Exception e){
@@ -71,14 +64,20 @@ public class CourseController {
         }
         return "redirect:/courses";
     }
+
     @GetMapping("/{courseId}/modules/delete/{moduleId}")
     public String borrarModulo(@PathVariable Long courseId, @PathVariable Long moduleId){
         courseService.borrarModulo(moduleId);
-        return "redirect:/courses/edit" + courseId;
+
+        return "redirect:/courses/edit/" + courseId;
     }
-    @PostMapping("{courseId}/modules/update/{moduleId}")
-    public String actualizarModulo(@PathVariable Long courseId, @PathVariable Long moduleId, @RequestParam("title")String title){
+
+    @PostMapping("/{courseId}/modules/update/{moduleId}")
+    public String actualizarModulo(@PathVariable Long courseId,
+                                   @PathVariable Long moduleId,
+                                   @RequestParam("title") String title){
         courseService.actualizarModulo(moduleId, title);
-        return "redirect:/courses/edit" + courseId;
+
+        return "redirect:/courses/edit/" + courseId;
     }
 }
